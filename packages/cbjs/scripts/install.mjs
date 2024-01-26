4182cb0b44d066cab3443d2552cc6e6851ca64a3
Jonathan MASSUCHETTI <jonathan.massuchetti@dappit.fr>
2024-01-27 00:05:03 +0100

    fix(npm): update install script after moving to a monorepo

--- packages/cbjs/scripts/install.mjs
+++ packages/cbjs/scripts/install.mjs
@@ -3,27 +3,22 @@
  */
 import fs from 'node:fs';
 import path from 'node:path';
-import { spawn } from 'node:child_process';
 import { downloadBinary } from './downloadBinary.mjs';
-import { isElectron, isAlpine, getNodeVersion, getNodeMajorVersion, getSSLType, getLinuxType } from './utils.mjs';
 
-const packageDir = process.cwd();
-const isProjectDev = process.env.INIT_CWD === undefined || (process.env.INIT_CWD === process.cwd());
+const packageAbsolutePath = process.cwd();
+const packageRelative = 'packages/cbjs';
+const isProjectDev = process.env.INIT_CWD === undefined || (packageAbsolutePath.substring(process.env.INIT_CWD.length) === `/${packageRelative}`);
 
-const runtime = isElectron() ? 'electron' : 'node';
-const nodeVersion = getNodeVersion();
-const nodeVersionMajor = getNodeMajorVersion(nodeVersion);
 const arch = process.arch;
 const platform = process.platform;
-const libc = getLinuxType(platform);
 const sslType = 'boringssl';
 
 const binaryPackageName = `couchbase-${platform}-${arch}-napi`;
 const binaryPackageVersion = process.env.COUCHBASE_BINARY_VERSION || process.argv[2];
 const binarySourcePath = `package/couchbase-v${binaryPackageVersion}-napi-v6-${platform}-${arch}-${sslType}.node`;
 const binaryDestinationPath = isProjectDev ?
-  path.resolve(packageDir, `src/couchbase-native.node`) :
-  path.resolve(packageDir, 'dist/couchbase-native.node');
+  path.resolve(packageAbsolutePath, `src/couchbase-native.node`) :
+  path.resolve(packageAbsolutePath, 'dist/couchbase-native.node');
 
 if (fs.existsSync(binaryDestinationPath)) {
   console.info(`Couchbase binary is already installed in ${binaryDestinationPath}`)
