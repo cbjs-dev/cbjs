bfaa5ef814e22553ac1141d11ccd2afac9a2849f
Jonathan MASSUCHETTI <jonathan.massuchetti@dappit.fr>
2024-04-16 18:48:19 +0200

    chore: rename script

--- tests/scripts/createCouchbaseContainer.sh
+++ tests/scripts/createCouchbaseContainer.sh
@@ -1,5 +1,6 @@
-docker pull couchbase
+docker pull couchbase:7.2.4
 docker run \
+  --name couchbase-7.2.4 \
   --detach \
   -p 8091:8091 \
   -p 8092:8092 \
@@ -12,4 +13,4 @@ docker run \
   -p 9123:9123 \
   -p 11210:11210 \
   -p 11280:11280 \
-  couchbase
+  couchbase:7.2.4
