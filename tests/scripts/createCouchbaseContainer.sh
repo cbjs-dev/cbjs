826bfe33d5d3efb4ac5a00b80e3641fb37888075
Jonathan MASSUCHETTI <jonathan.massuchetti@dappit.fr>
2024-04-16 21:43:17 +0200

    build: update script to create couchbase container

--- tests/scripts/createCouchbaseContainer.sh
+++ tests/scripts/createCouchbaseContainer.sh
@@ -1,6 +1,6 @@
-docker pull couchbase:7.2.4
+docker pull couchbase:7.6.1
 docker run \
-  --name couchbase-7.2.4 \
+  --name couchbase-7.6.1 \
   --detach \
   -p 8091:8091 \
   -p 8092:8092 \
@@ -13,4 +13,4 @@ docker run \
   -p 9123:9123 \
   -p 11210:11210 \
   -p 11280:11280 \
-  couchbase:7.2.4
+  couchbase:7.6.1
