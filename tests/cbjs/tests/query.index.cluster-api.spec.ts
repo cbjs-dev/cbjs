6854615cabae9a105d20ce6e0eba62798a1bb67d
Jonathan MASSUCHETTI <jonathan.massuchetti@dappit.fr>
2024-05-01 11:34:01 +0200

    test(query): increase primary index waiting time

--- tests/cbjs/tests/query.index.cluster-api.spec.ts
+++ tests/cbjs/tests/query.index.cluster-api.spec.ts
@@ -54,206 +54,212 @@ describe.runIf(serverSupportsFeatures(ServerFeatures.Query, ServerFeatures.Colle
       },
     ];
 
-    describe.each(cases)('$name', ({ getDataCollection, getIndexKeyspace }) => {
-      test('should successfully create & drop a primary index', async ({
-        serverTestContext,
-        usePrimaryIndex,
-      }) => {
-        await usePrimaryIndex(getIndexKeyspace(serverTestContext));
-      });
-
-      test('should throw a IndexExistsError when creating a duplicate primary index with the same name', async function ({
-        expect,
-        usePrimaryIndex,
-        serverTestContext,
-      }) {
-        expect.hasAssertions();
-        const indexName = await usePrimaryIndex(getIndexKeyspace(serverTestContext));
-
-        try {
-          await usePrimaryIndex({
-            ...getIndexKeyspace(serverTestContext),
-            name: indexName,
+    describe.each(cases)(
+      '$name',
+      { timeout: 30_000 },
+      ({ getDataCollection, getIndexKeyspace }) => {
+        test('should successfully create & drop a primary index', async ({
+          serverTestContext,
+          usePrimaryIndex,
+        }) => {
+          await usePrimaryIndex(getIndexKeyspace(serverTestContext), {
+            waitIndexTimeout: 30_000,
           });
-        } catch (err) {
-          expect(err).toBeInstanceOf(IndexExistsError);
-          invariant(err instanceof IndexExistsError);
-          expect(err.context).toBeInstanceOf(HttpErrorContext);
-        }
-      });
-
-      test('should successfully create & drop secondary index (using a new connection)', async function ({
-        useIndex,
-        serverTestContext,
-      }) {
-        await useIndex({
-          ...getIndexKeyspace(serverTestContext),
-          fields: ['a'],
-        });
-      });
-
-      test('should throw a IndexExistsError when creating a duplicate secondary index with the same name', async function ({
-        useIndex,
-        expect,
-        serverTestContext,
-      }) {
-        expect.hasAssertions();
-        const indexName = await useIndex({
-          ...getIndexKeyspace(serverTestContext),
-          fields: ['a'],
         });
 
-        try {
+        test('should throw a IndexExistsError when creating a duplicate primary index with the same name', async function ({
+          expect,
+          usePrimaryIndex,
+          serverTestContext,
+        }) {
+          expect.hasAssertions();
+          const indexName = await usePrimaryIndex(getIndexKeyspace(serverTestContext));
+
+          try {
+            await usePrimaryIndex({
+              ...getIndexKeyspace(serverTestContext),
+              name: indexName,
+            });
+          } catch (err) {
+            expect(err).toBeInstanceOf(IndexExistsError);
+            invariant(err instanceof IndexExistsError);
+            expect(err.context).toBeInstanceOf(HttpErrorContext);
+          }
+        });
+
+        test('should successfully create & drop secondary index (using a new connection)', async function ({
+          useIndex,
+          serverTestContext,
+        }) {
           await useIndex({
-            name: indexName,
             ...getIndexKeyspace(serverTestContext),
             fields: ['a'],
           });
-        } catch (err) {
-          expect(err).toBeInstanceOf(IndexExistsError);
-          invariant(err instanceof IndexExistsError);
-          expect(err.context).toBeInstanceOf(HttpErrorContext);
-        }
-      });
-
-      test('should successfully get all indexes', async function ({
-        serverTestContext,
-        useIndex,
-        usePrimaryIndex,
-        expect,
-      }) {
-        const primaryIndexName = await usePrimaryIndex(
-          getIndexKeyspace(serverTestContext)
-        );
-        const secondaryIndexName = await useIndex({
-          ...getIndexKeyspace(serverTestContext),
-          fields: ['a'],
-        });
-        await expect(
-          serverTestContext.cluster
-            .queryIndexes()
-            .getAllIndexes(serverTestContext.bucket.name)
-        ).resolves.toEqual([
-          expect.objectContaining({ name: primaryIndexName }),
-          expect.objectContaining({ name: secondaryIndexName }),
-        ]);
-      });
-
-      test('should throw a IndexNotFoundError when watching when some missing indexes', async function ({
-        expect,
-        serverTestContext,
-      }) {
-        expect.hasAssertions();
-
-        try {
-          await serverTestContext.cluster
-            .queryIndexes()
-            .watchIndexes(
-              getIndexKeyspace(serverTestContext).bucketName,
-              ['missingIndex'],
-              2000
-            );
-        } catch (err) {
-          expect(err).toBeInstanceOf(IndexNotFoundError);
-          invariant(err instanceof IndexNotFoundError);
-          // JSCBC-1230
-          // expect(err.context).toBeInstanceOf(HttpErrorContext);
-        }
-      });
-
-      test('should throw a IndexNotFoundError when dropping a missing secondary index', async function ({
-        expect,
-        serverTestContext,
-      }) {
-        expect.hasAssertions();
-
-        try {
-          await serverTestContext.cluster
-            .queryIndexes()
-            .dropIndex(getIndexKeyspace(serverTestContext).bucketName, 'missingIndex');
-        } catch (err) {
-          expect(err).toBeInstanceOf(IndexNotFoundError);
-          invariant(err instanceof IndexNotFoundError);
-          expect(err.context).toBeInstanceOf(HttpErrorContext);
-        }
-      });
-
-      test('should build deferred indexes', async function ({
-        expect,
-        serverTestContext,
-        useSampleData,
-        useIndex,
-      }) {
-        await useSampleData(getDataCollection(serverTestContext) as any);
-
-        const keyspaceProps = getIndexKeyspace(serverTestContext);
-
-        const indexName0 = await useIndex({
-          ...keyspaceProps,
-          fields: ['name'],
-          deferred: true,
         });
 
-        const indexName1 = await useIndex({
-          ...keyspaceProps,
-          fields: ['name', 'testUid'],
-          deferred: true,
-        });
-
-        const indexes = await serverTestContext.cluster
-          .queryIndexes()
-          .getAllIndexes(keyspaceProps.bucketName);
-
-        expect(indexes).toHaveLength(2);
-        expect(indexes).toContainEqual(
-          expect.objectContaining({ name: indexName0, state: 'deferred' })
-        );
-        expect(indexes).toContainEqual(
-          expect.objectContaining({ name: indexName1, state: 'deferred' })
-        );
-
-        await serverTestContext.cluster
-          .queryIndexes()
-          .buildDeferredIndexes(keyspaceProps.bucketName, {
-            scopeName: keyspaceProps.scopeName,
-            collectionName: keyspaceProps.collectionName,
+        test('should throw a IndexExistsError when creating a duplicate secondary index with the same name', async function ({
+          useIndex,
+          expect,
+          serverTestContext,
+        }) {
+          expect.hasAssertions();
+          const indexName = await useIndex({
+            ...getIndexKeyspace(serverTestContext),
+            fields: ['a'],
           });
 
-        await serverTestContext.cluster
-          .queryIndexes()
-          .watchIndexes(keyspaceProps.bucketName, [indexName0, indexName1], 6000);
+          try {
+            await useIndex({
+              name: indexName,
+              ...getIndexKeyspace(serverTestContext),
+              fields: ['a'],
+            });
+          } catch (err) {
+            expect(err).toBeInstanceOf(IndexExistsError);
+            invariant(err instanceof IndexExistsError);
+            expect(err.context).toBeInstanceOf(HttpErrorContext);
+          }
+        });
 
-        const indexesBuilt = await serverTestContext.cluster
-          .queryIndexes()
-          .getAllIndexes(keyspaceProps.bucketName);
+        test('should successfully get all indexes', async function ({
+          serverTestContext,
+          useIndex,
+          usePrimaryIndex,
+          expect,
+        }) {
+          const primaryIndexName = await usePrimaryIndex(
+            getIndexKeyspace(serverTestContext)
+          );
+          const secondaryIndexName = await useIndex({
+            ...getIndexKeyspace(serverTestContext),
+            fields: ['a'],
+          });
+          await expect(
+            serverTestContext.cluster
+              .queryIndexes()
+              .getAllIndexes(serverTestContext.bucket.name)
+          ).resolves.toEqual([
+            expect.objectContaining({ name: primaryIndexName }),
+            expect.objectContaining({ name: secondaryIndexName }),
+          ]);
+        });
 
-        expect(indexesBuilt).toHaveLength(2);
-        expect(indexesBuilt).toContainEqual(
-          expect.objectContaining({ name: indexName0, state: 'online' })
-        );
-        expect(indexesBuilt).toContainEqual(
-          expect.objectContaining({ name: indexName1, state: 'online' })
-        );
-      });
+        test('should throw a IndexNotFoundError when watching when some missing indexes', async function ({
+          expect,
+          serverTestContext,
+        }) {
+          expect.hasAssertions();
 
-      test('should throw a IndexNotFoundError when dropping a missing primary index', async function ({
-        expect,
-        serverTestContext,
-      }) {
-        expect.hasAssertions();
+          try {
+            await serverTestContext.cluster
+              .queryIndexes()
+              .watchIndexes(
+                getIndexKeyspace(serverTestContext).bucketName,
+                ['missingIndex'],
+                2000
+              );
+          } catch (err) {
+            expect(err).toBeInstanceOf(IndexNotFoundError);
+            invariant(err instanceof IndexNotFoundError);
+            // JSCBC-1230
+            // expect(err.context).toBeInstanceOf(HttpErrorContext);
+          }
+        });
 
-        try {
+        test('should throw a IndexNotFoundError when dropping a missing secondary index', async function ({
+          expect,
+          serverTestContext,
+        }) {
+          expect.hasAssertions();
+
+          try {
+            await serverTestContext.cluster
+              .queryIndexes()
+              .dropIndex(getIndexKeyspace(serverTestContext).bucketName, 'missingIndex');
+          } catch (err) {
+            expect(err).toBeInstanceOf(IndexNotFoundError);
+            invariant(err instanceof IndexNotFoundError);
+            expect(err.context).toBeInstanceOf(HttpErrorContext);
+          }
+        });
+
+        test('should build deferred indexes', async function ({
+          expect,
+          serverTestContext,
+          useSampleData,
+          useIndex,
+        }) {
+          await useSampleData(getDataCollection(serverTestContext) as any);
+
+          const keyspaceProps = getIndexKeyspace(serverTestContext);
+
+          const indexName0 = await useIndex({
+            ...keyspaceProps,
+            fields: ['name'],
+            deferred: true,
+          });
+
+          const indexName1 = await useIndex({
+            ...keyspaceProps,
+            fields: ['name', 'testUid'],
+            deferred: true,
+          });
+
+          const indexes = await serverTestContext.cluster
+            .queryIndexes()
+            .getAllIndexes(keyspaceProps.bucketName);
+
+          expect(indexes).toHaveLength(2);
+          expect(indexes).toContainEqual(
+            expect.objectContaining({ name: indexName0, state: 'deferred' })
+          );
+          expect(indexes).toContainEqual(
+            expect.objectContaining({ name: indexName1, state: 'deferred' })
+          );
+
           await serverTestContext.cluster
             .queryIndexes()
-            .dropPrimaryIndex(getIndexKeyspace(serverTestContext).bucketName, {
-              name: 'missingIndex',
+            .buildDeferredIndexes(keyspaceProps.bucketName, {
+              scopeName: keyspaceProps.scopeName,
+              collectionName: keyspaceProps.collectionName,
             });
-        } catch (err) {
-          expect(err).toBeInstanceOf(IndexNotFoundError);
-          invariant(err instanceof IndexNotFoundError);
-          expect(err.context).toBeInstanceOf(HttpErrorContext);
-        }
-      });
-    });
+
+          await serverTestContext.cluster
+            .queryIndexes()
+            .watchIndexes(keyspaceProps.bucketName, [indexName0, indexName1], 6000);
+
+          const indexesBuilt = await serverTestContext.cluster
+            .queryIndexes()
+            .getAllIndexes(keyspaceProps.bucketName);
+
+          expect(indexesBuilt).toHaveLength(2);
+          expect(indexesBuilt).toContainEqual(
+            expect.objectContaining({ name: indexName0, state: 'online' })
+          );
+          expect(indexesBuilt).toContainEqual(
+            expect.objectContaining({ name: indexName1, state: 'online' })
+          );
+        });
+
+        test('should throw a IndexNotFoundError when dropping a missing primary index', async function ({
+          expect,
+          serverTestContext,
+        }) {
+          expect.hasAssertions();
+
+          try {
+            await serverTestContext.cluster
+              .queryIndexes()
+              .dropPrimaryIndex(getIndexKeyspace(serverTestContext).bucketName, {
+                name: 'missingIndex',
+              });
+          } catch (err) {
+            expect(err).toBeInstanceOf(IndexNotFoundError);
+            invariant(err instanceof IndexNotFoundError);
+            expect(err.context).toBeInstanceOf(HttpErrorContext);
+          }
+        });
+      }
+    );
   }
 );
