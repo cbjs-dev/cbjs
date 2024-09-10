26ced58fd4b5a9c3f8a27eb0baa56a3ffa262aa4
Jonathan MASSUCHETTI <jonathan.massuchetti@dappit.fr>
2024-09-10 14:42:25 +0200

    test(cbjs): remove incorrect `CouchbaseCas.isCasInput` assertion

--- packages/shared/src/couchbase/CouchbaseCas.spec.ts
+++ packages/shared/src/couchbase/CouchbaseCas.spec.ts
@@ -47,10 +47,6 @@ describe('CouchbaseCas', () => {
     expect(CouchbaseCas.from(0)).toHaveProperty('raw');
   });
 
-  it('should consider a Uint8Array to be a valid CasInput', ({ expect }) => {
-    expect(CouchbaseCas.isCasInput(new Uint8Array())).toBe(true);
-  });
-
   it('should consider a Buffer to be a valid CasInput', ({ expect }) => {
     expect(CouchbaseCas.isCasInput(Buffer.of(0))).toBe(true);
   });
