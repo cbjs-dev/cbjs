921c0bc442d092e56f47f003cbfda4f0cb32d86d
Jonathan MASSUCHETTI <jonathan.massuchetti@dappit.fr>
2026-03-12 19:52:57 +0100

    test: run if server support scope search feature

--- tests/http-client/tests/waitFor/waitForDocumentsInSearchIndex.spec.ts
+++ tests/http-client/tests/waitFor/waitForDocumentsInSearchIndex.spec.ts
@@ -15,77 +15,83 @@
  */
 import { describe } from 'vitest';
 
+import { ServerFeatures } from '@cbjsdev/http-client';
 import { waitForDocumentsInSearchIndex } from '@cbjsdev/http-client/dist/src/waitFor/waitForDocumentsInSearchIndex.js';
 import { getRandomId } from '@cbjsdev/shared';
 import { createCouchbaseTest } from '@cbjsdev/vitest';
 
+import { serverSupportsFeatures } from '../../utils/serverFeature.js';
 import getIndexDefinition from './getIndexDefinition.js';
 
-describe('waitForDocumentsInSearchIndex', { timeout: 100_000 }, async () => {
-  const test = await createCouchbaseTest();
+describe.runIf(serverSupportsFeatures(ServerFeatures.ScopeSearchIndexManagement))(
+  'waitForDocumentsInSearchIndex',
+  { timeout: 100_000 },
+  async () => {
+    const test = await createCouchbaseTest();
 
-  test(
-    'should return once the document are found',
-    async ({
-      expect,
-      useScopedSearchIndex,
-      useDocumentKey,
-      useScope,
-      useCollection,
-      serverTestContext,
-      apiConfig,
-    }) => {
-      const scopeName = await useScope();
-      const collectionName = await useCollection({ scopeName });
-
-      const searchIndexName = `searchIndex` + getRandomId();
-      const def = getIndexDefinition(searchIndexName, {
-        bucket: serverTestContext.bucket.name,
-        scope: scopeName,
-        collection: collectionName,
-      });
-
-      const indexName = await useScopedSearchIndex(
-        {
-          bucketName: serverTestContext.bucket.name,
-          scopeName,
-          collectionName,
-          ...def,
-        },
-        { waitSearchIndexTimeout: 100_000, awaitMutations: true }
-      );
-
-      const testDocKey = useDocumentKey();
-      const testDocBody = {
-        title: 'Couchbase',
-        tagline: 'No Equal - The Cloud database for modern applications',
-        description:
-          'Couchbase is a NoSQL, distributed database focused on performances at scale.',
-      };
-
-      const testDocKey2 = useDocumentKey();
-      const testDocBody2 = {
-        title: 'Magma',
-        description: 'The latest storage engine from Couchbase.',
-      };
-
-      const collection = serverTestContext.bucket
-        .scope(scopeName)
-        .collection(collectionName);
-      await collection.insert(testDocKey, testDocBody);
-      await collection.insert(testDocKey2, testDocBody2);
-
-      await waitForDocumentsInSearchIndex(
+    test(
+      'should return once the document are found',
+      async ({
+        expect,
+        useScopedSearchIndex,
+        useDocumentKey,
+        useScope,
+        useCollection,
+        serverTestContext,
         apiConfig,
-        {
+      }) => {
+        const scopeName = await useScope();
+        const collectionName = await useCollection({ scopeName });
+
+        const searchIndexName = `searchIndex` + getRandomId();
+        const def = getIndexDefinition(searchIndexName, {
           bucket: serverTestContext.bucket.name,
           scope: scopeName,
-          index: indexName,
-        },
-        [testDocKey, testDocKey2],
-        { timeout: 60_000 }
-      );
-    },
-    { timeout: 100_000 }
-  );
-});
+          collection: collectionName,
+        });
+
+        const indexName = await useScopedSearchIndex(
+          {
+            bucketName: serverTestContext.bucket.name,
+            scopeName,
+            collectionName,
+            ...def,
+          },
+          { waitSearchIndexTimeout: 100_000, awaitMutations: true }
+        );
+
+        const testDocKey = useDocumentKey();
+        const testDocBody = {
+          title: 'Couchbase',
+          tagline: 'No Equal - The Cloud database for modern applications',
+          description:
+            'Couchbase is a NoSQL, distributed database focused on performances at scale.',
+        };
+
+        const testDocKey2 = useDocumentKey();
+        const testDocBody2 = {
+          title: 'Magma',
+          description: 'The latest storage engine from Couchbase.',
+        };
+
+        const collection = serverTestContext.bucket
+          .scope(scopeName)
+          .collection(collectionName);
+        await collection.insert(testDocKey, testDocBody);
+        await collection.insert(testDocKey2, testDocBody2);
+
+        await waitForDocumentsInSearchIndex(
+          apiConfig,
+          {
+            bucket: serverTestContext.bucket.name,
+            scope: scopeName,
+            index: indexName,
+          },
+          [testDocKey, testDocKey2],
+          { timeout: 60_000 }
+        );
+      },
+      { timeout: 100_000 }
+    );
+  }
+);
